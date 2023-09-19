df23ecb9327ec942d55d24a198835aa40f91bc74
Lucilary <gofafafa1@gmail.com>
2023-09-19 10:27:03 +0700

    test

---
+++ AlchemiaGui/src/main/java/com/mycompany/alchemiagui/AlchemiaGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alchemiagui;
+
+/**
+ *
+ * @author Lucilary
+ */
+public class AlchemiaGui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
